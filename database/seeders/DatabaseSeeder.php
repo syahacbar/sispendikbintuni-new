76abe39f7a1c8bfba9352703066c3b85d22c7689
ptadcmanokwari <syahacbar@gmail.com>
2025-07-08 22:53:12 +0900

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,6 @@ class DatabaseSeeder extends Seeder
             JenisSarprasSeeder::class,
             KelompokMapelSeeder::class,
             KelompokMapelSeeder::class,
-            WilayahSeeder::class,
             PengaduanSeeder::class,
             PengaturanSeeder::class,
         ]);
