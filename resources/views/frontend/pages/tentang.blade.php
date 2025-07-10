891693ed356f66a0bb6684d83c3ecb3a8f83bd1a
CSS Coder <csscoder95@gmail.com>
2025-07-10 19:26:02 +0900

    Update tentang.blade.php

--- resources/views/frontend/pages/tentang.blade.php
+++ resources/views/frontend/pages/tentang.blade.php
@@ -4,17 +4,19 @@
     <section class="about-section">
         <div class="container">
             <div class="row my-3">
-                <div class="card-header">
-                    <h6>Tentang Web</h6>
-                </div>
-                <div class="card mt-3">
-                    <div class="card-body">
-                        @forelse($tentangData as $key => $value)
-                            <h4 class="text-capitalize">{{ str_replace('_', ' ', $key) }}</h4>
-                            <div>{!! $value !!}</div>
-                        @empty
-                            <p>Konten tentang belum tersedia.</p>
-                        @endforelse
+                <div class="card">
+                    <div class="card-header">
+                        <h6>Tentang Web</h6>
+                    </div>
+                    <div class="card mt-3">
+                        <div class="card-body">
+                            @forelse($tentangData as $key => $value)
+                                <h4 class="text-capitalize">{{ str_replace('_', ' ', $key) }}</h4>
+                                <div>{!! $value !!}</div>
+                            @empty
+                                <p>Konten tentang belum tersedia.</p>
+                            @endforelse
+                        </div>
                     </div>
                 </div>
             </div>
