4cbe995a81f34e9055e98dac4fd42349648d301e
ptadcmanokwari <syahacbar@gmail.com>
2025-07-13 22:28:46 +0900

    endpoint api untuk sebaran

---
+++ app/Http/Controllers/Api/PetaSebaranController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class PetaSebaranController extends Controller
+{
+    public function index()
+    {
+        $data = DB::table('tbl_sekolahs')
+            ->select('nama', 'jenjang', 'latitude as lat', 'longitude as lng')
+            ->whereNotNull('latitude')
+            ->whereNotNull('longitude')
+            ->get();
+
+        return response()->json($data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\WilayahController;
 use App\Http\Controllers\Api\SekolahController;
+use App\Http\Controllers\Api\PetaSebaranController;
 
 Route::get('/distriklist', [WilayahController::class, 'distrikList']);
 Route::get('/sekolah/by-distrik/{kode_wilayah}', [SekolahController::class, 'byDistrik']);
 Route::get('sekolah/detail/{id}', [SekolahController::class, 'detail']);
+Route::get('/sebaran', [PetaSebaranController::class, 'index']);
